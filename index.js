e81a9c9cbfc578e08675b935435359a641d0cc48
Faizan Sayyed <faizansayyed36@gmail.com>
2022-10-01 10:32:50 +0530

    Query with Variables (get products, products by id)

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { products } = require("./initial_data");
 
 const typeDefs = gql`
     type Query {
       hello: String
+      products: [Product!]!
+      product(id:ID!): Product
+    }
+
+    type Product {
+      name: String!,
+      description: String!,
+      quantity: Int!,
+      price: Float!,
+      onSale: Boolean!,
     }
 `
 
@@ -10,9 +21,18 @@ const resolvers = {
   Query: {
     hello: () => {
       return "World!"
+    },
+    products: () => products,
+    product: (parent, args, context) => {
+     
+      const productId = args.id;
+      const product = products.find(d => d.id === productId)
+      if (!product) return null;
+      return product
     }
   }
 }
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
